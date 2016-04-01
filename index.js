c97db9d27e79f28d7dee690ec5b1a91dc9b0fe7f
Matheus Alves <theuves@outlook.com>
2016-03-31 22:25:23 -0400

    Para Node.js

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = (function () {
+    var palavroes = [
+        'po+rr+(i+nha+|a+)',
+        '(c|k)a+ra+lh(a+da+|(ã|a)o|i+nho+|o+)',
+        'pu+t(a+ri+a+|i+nh(o|a)+|a+da+|(ã|a)o|a+|o+)',
+        'b(u|o)+(c|s)e+t(u+d(a|o)+|a+da+|(ã|a)+o+|i+nha+|a+)',
+        '(c|k)(ú|u)((z|s)(ã|a)+o+|(z|s)i+nho+|(z|s)u+do+)?',
+        'pi+nt(u+do+|ã+o+|o+)',
+        'fo+d(i+do+|e+r|e+mo+s|e+r(ã+o+|a+m)|e+re+(i+|mo+s)|e+ndo+|a(-se+)?|(e|o|i)+?)',
+        'fu+d(i+do+|e+r|e+mo+s|e+r(ã+o+|a+m)|e+re+(i+|mo+s)|e+ndo+|i+)'
+    ];
+
+    for (var i in palavroes) {
+        palavroes[i] = '(\\b' + palavroes[i] + '\\b)';
+    }
+
+    var regexp = new RegExp(palavroes.join('|'), 'gi');
+
+    return {
+        extrair: function (str) {
+            if (typeof str === 'string') return str.match(regexp) || [];
+        },
+        substituir: function (str, subs) {
+            if (typeof string === 'string' && subs) return str.replace(regexp, subs);
+        },
+        verificar: function (str) {
+            if (typeof str === 'string') {
+                if (str.match(regexp)) return true;
+                return false
+            }
+        }
+    };
+})();
