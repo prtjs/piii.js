6ce24783b9921c59f7f464c5aae8c2afadac05e6
Matheus Alves <theuves@outlook.com>
2016-04-04 17:06:51 -0400

    Algumas alterações.

--- piii.js
+++ piii.js
@@ -1,35 +1,34 @@
 'use strict';
 
 var Piii = (function () {
-    var palavroes = [
-        'po+rr+(i+nha+|a+)',
-        '(c|k)a+ra+lh(a+da+|(ã|a)o|i+nho+|o+)',
-        'pu+t(a+ri+a+|i+nh(o|a)+|a+da+|(ã|a)o|a+|o+)',
-        'b(u|o)+(c|s)e+t(u+d(a|o)+|a+da+|(ã|a)+o+|i+nha+|a+)',
-        '(c|k)(ú|u)((z|s)(ã|a)+o+|(z|s)i+nho+|(z|s)u+do+)?',
-        'pi+nt(u+do+|ã+o+|o+)',
-        'fo+d(i+do+|e+r|e+mo+s|e+r(ã+o+|a+m)|e+re+(i+|mo+s)|e+ndo+|a(-se+)?|(e|o|i)+?)',
-        'fu+d(i+do+|e+r|e+mo+s|e+r(ã+o+|a+m)|e+re+(i+|mo+s)|e+ndo+|i+)'
+    var plvrs = [
+        'po(rr|h)(a|inha(zinha)?|ona|ão)', 
+        'caralh(o(zinho|zão)?|inho(zinho)?|ão|ada)',
+        'carai(o(zinho|zão)?|nho(zinho)?|ão|ada)',
+        'put(a|o|inha(zinha)?|ona|ão)',
+        'b(u|o)cet(a(zona|zinha)?|inha(zinha)?|ona|ão)',
+        '(c|k)(ú|u|uh|uzinho|uzão|uzona)',
+        'pint(o(zinho|zão)?|ão)',
+        'f(o|u)d(er|emos)',
+        'fod(a|inha|ão|er|o|es|e|emos|eis|em)'
     ];
 
-    for (var i in palavroes) {
-        palavroes[i] = '(\\b' + palavroes[i] + '\\b)';
+    for (var i in plvrs) {
+        plvrs[i] = plvrs[i].replace(/([\wâàáãêéíõôóûúüç])/g, '$1+');
+        plvrs[i] = '(\\b' + plvrs[i] + '\\b)';
     }
 
-    var regexp = new RegExp(palavroes.join('|'), 'gi');
+    var regex = new RegExp(plvrs.join('|'), 'gi');
 
     return {
         extrair: function (str) {
-            if (typeof str === 'string') return str.match(regexp) || [];
+            return str.match(regex) || [];
         },
-        substituir: function (str, subst) {
-            if (typeof str === 'string' && subst) return str.replace(regexp, subst);
+        substituir: function (str, sbsttt) {
+            return str.replace(regex, sbsttt);
         },
         verificar: function (str) {
-            if (typeof str === 'string') {
-                if (str.match(regexp)) return true;
-                return false;
-            }
+            return str.match(regex) ? true : false;
         }
     };
 })();
