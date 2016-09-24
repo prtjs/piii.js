bbc032082a05fa4526982f71f2f7eece07dcd152
Matheus Alves <theuves@outlook.com>
2016-09-24 13:14:10 -0400

    Remove um teste

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ import piii from './';
 test('piii', t => {
     t.is(piii('Vá tomar no cú!'), 'Vá tomar no **!');
     t.is(piii('Vá se ⓕⓞⓓⓔⓡ!'), 'Vá se *****!');
-    t.is(piii('Que m3rd4!'), 'Que *****!');
     t.is(piii('Seu fdp!'), 'Seu ***!');
     t.is(piii('Cúzãozãozão'), '***********');
     t.is(piii('Filho da ᵽṻțặ!'), 'Filho da ****!');
