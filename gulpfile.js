55cff4db01d3a40c8a73125d04a9b27d3a3b6c2e
Matheus Alves <theuves@outlook.com>
2017-03-09 21:11:07 -0400

    algumas alteracoes

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,11 @@ const source = require('vinyl-source-stream');
 const browserify = require('browserify');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const banner = require('browserify-banner');
 
 gulp.task('browserify', () => {
     return browserify('./src/piii.js', {standalone: 'piii'})
+        .plugin(banner, {file: './.banner.txt'})
         .bundle()
         .pipe(source('piii.js'))
         .pipe(gulp.dest('./dist/'));
@@ -15,7 +17,7 @@ gulp.task('browserify', () => {
 
 gulp.task('uglify', ['browserify'], () => {
     return gulp.src('./dist/piii.js')
-        .pipe(uglify())
+        .pipe(uglify({output: {comments: '/^!.*$/'}}))
         .pipe(rename('piii.min.js'))
         .pipe(gulp.dest('./dist/'));
 });
