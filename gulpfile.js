1437d84e75d0d7f96b2598bef9fc0b671f6cbb26
Matheus Alves <theuves@outlook.com>
2017-05-10 21:55:36 -0400

    aspas duplas e outras alterações

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const source = require('vinyl-source-stream');
-const browserify = require('browserify');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const banner = require('browserify-banner');
+var gulp = require("gulp");
+var source = require("vinyl-source-stream");
+var browserify = require("browserify");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var banner = require("browserify-banner");
 
-gulp.task('browserify', () => {
-    return browserify('./src/piii.js', {standalone: 'piii'})
-        .plugin(banner, {file: './.banner.txt'})
-        .bundle()
-        .pipe(source('piii.js'))
-        .pipe(gulp.dest('./dist/'));
+gulp.task("browserify", function () {
+  return browserify("./src/piii.js", {standalone: "piii"})
+    .plugin(banner, {file: "./.banner.txt"})
+    .bundle()
+    .pipe(source("piii.js"))
+    .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('uglify', ['browserify'], () => {
-    return gulp.src('./dist/piii.js')
-        .pipe(uglify({output: {comments: '/^!.*$/'}}))
-        .pipe(rename('piii.min.js'))
-        .pipe(gulp.dest('./dist/'));
+gulp.task("uglify", ["browserify"], function () {
+  return gulp.src("./dist/piii.js")
+    .pipe(uglify({output: {comments: "/^!.*$/"}}))
+    .pipe(rename("piii.min.js"))
+    .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('default', ['uglify']);
+gulp.task("default", ["uglify"]);
