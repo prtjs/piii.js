bd67756f8c257bbf5c34f645b0902a5a5bd44030
Matheus Alves <theuves@outlook.com>
2017-05-12 19:43:09 -0400

    novo piii.js

--- src/piii.js
+++ src/piii.js
@@ -1,87 +1,57 @@
-'use strict';
+"use strict";
 
-// importa dependências
-var diacritics = require('diacritics');
-var palavroes = require('./words.js');
-var repeat = require('repeat-string');
+var desacentuar = require("diacritics").remove;
+var regex = require("./regex.js");
 
-// piii.js - veja a documentação em:
-// <https://github.com/theuves/piii.js#readme>
-module.exports = function (string, options) {
-    if (!string) return;
-    options = options || {};
+module.exports = function (objeto) {
+  var censura = objeto.censura || "*";
+  var excecoes = objeto.excecoes || [];
 
-    var censura = options.censura || '*';
-    var completo = options.completo || false;
-    var extras = options.extras || [];
+  censura = censura.toString();
+  excecoes = Array.isArray(excecoes)
+    ? excecoes
+    : [];
 
-    // transforma todos os valores em string
-    // e remove a acentuação
-    extras = extras.map(function (extra) {
-        return diacritics.remove(extra.toString());
+  // Expressão regular para filtrar palavrões.
+  var filtro = regex(excecoes);
+
+  this.filtrar = function (string, substituto) {
+    string = string.toString();
+
+    substituto = typeof substituto === "function"
+      ? substituto
+      : function () {
+      return censura;
+    };
+
+    var desacentuada = desacentuar(string);
+
+    // Lista de palavrões (sem acentos).
+    var listaDePalavroes = desacentuada.match(filtro) || [];
+
+    if (!listaDePalavroes.length) {
+      return string;
+    }
+
+    // Expressão regular somente com os palavrões encontrados.
+    var palavroesRegex = "\\b(" + listaDePalavroes.join("|") + ")\\b";
+    palavroesRegex = new RegExp(palavroesRegex);
+
+    var dividido = desacentuada.split(palavroesRegex);
+    var nova = "";
+
+    var indice = 0;
+
+    dividido.forEach(function (parte) {
+      var trecho = string.substring(indice, indice + parte.length)
+
+      nova += palavroesRegex.test(parte)
+        ? substituto(trecho)
+        : trecho;
+
+      indice += parte.length;
     });
 
-    var censuraTemp = '*';
-
-    if (!completo) {
-        censuraTemp = censura.charAt(0);
-    }
-
-    // string sem acentuação
-    var desacentuado = diacritics.remove(string);
-
-    // expressão regular para corresponder palavrões
-    var re = palavroes.ignorarLeet(palavroes.lista.concat(extras));
-    re = palavroes.criarRegExp(re);
-
-    // censura todos os palavrões, substituindo cada caractere
-    // do palavrão pelo caractere * (censuraTemp)
-    desacentuado = desacentuado.replace(re, function (correspondido) {
-        return repeat(censuraTemp, correspondido.length);
-    });
-
-    // nova string censurada
-    var str = '';
-
-    // navega por cada caractere de `string` e `desacentuado`
-    // esse processo recoloca as acentuações que foram removidas
-    // mais acima, para não haver erro com o "\b" na expressão
-    // regular do javascript
-    for (var i = 0; i < string.length; i++) {
-
-        // se o caractere for um caractere censurado, ele será
-        // adicionado em `str`
-        if (string[i] !== desacentuado[i] && desacentuado[i] === censuraTemp) {
-            str += desacentuado[i];
-        } else {
-
-            // caso contrário, o caracter original
-            // (que pode ter acentos) será adicionado
-            str += string[i];
-        }
-    }
-
-    // se for para censurar por uma sequência de caracteres
-    if (completo) {
-        var novaString = ''; // nova string censurada
-        var tmpCensurado = false; // temp
-
-        // navega por cada caractere da string original
-        string.split('').forEach(function (char, index) {
-
-            // @,@
-            if (char === str[index]) {
-                novaString += char;
-                tmpCensurado = false;
-            } else if (tmpCensurado === false) {
-                novaString += censura;
-                tmpCensurado = true;
-            }
-        });
-
-        return novaString;
-    }
-
-    // senão
-    return str;
+    return nova;
+  }
 };
