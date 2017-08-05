bc510e02883caa564a1d2a2e5578adb48c554348
Matheus Alves <theuves@outlook.com>
2017-08-05 11:59:05 -0400

    adiciona gulp-palavr

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 "use strict";
 
 var gulp = require("gulp");
-var source = require("vinyl-source-stream");
-var browserify = require("browserify");
-var uglify = require("gulp-uglify");
+
+// Gulp...
+var palavr = require("gulp-palavr");
 var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var watch = require("gulp-watch");
+
+// Outros...
 var banner = require("browserify-banner");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
 
 gulp.task("browserify", function () {
   return browserify("./src/piii.js", {standalone: "piii"})
@@ -13,6 +19,14 @@ gulp.task("browserify", function () {
     .bundle()
     .pipe(source("piii.js"))
     .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("regexs", function () {
+  return watch("./src/lista/regexs/**/*.palavr", function () {
+    return gulp.src("./src/lista/regexs/**/*.palavr")
+      .pipe(palavr("index.json"))
+      .pipe(gulp.dest("./src/lista/"));
+  });
 });
 
 gulp.task("uglify", ["browserify"], function () {
