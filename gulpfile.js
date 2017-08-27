76ebecfe65953e442fb6cb5e53174316f40d51b3
Matheus Alves <theuves@outlook.com>
2017-08-26 20:14:48 -0400

    alterações nas tarefas

--- gulpfile.js
+++ gulpfile.js
@@ -14,23 +14,23 @@ var banner = require("browserify-banner");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 
-gulp.task("browserify", function () {
-  return browserify("./src/piii.js", {standalone: "piii"})
-    .plugin(banner, {file: "./.banner.txt"})
-    .bundle()
-    .pipe(source("piii.js"))
-    .pipe(gulp.dest("./dist/"));
-});
-
 gulp.task("regexs", function () {
   return gulp.src("./src/lista/regexs/**/*.palavr")
     .pipe(palavr("index.json"))
     .pipe(gulp.dest("./src/lista/"));
 });
 
+gulp.task("browserify", ["regexs"], function () {
+  return browserify("./src/index.js", {standalone: "piii"})
+    .plugin(banner, {file: "./.banner.txt"})
+    .bundle()
+    .pipe(source("piii.js"))
+    .pipe(gulp.dest("./dist/"));
+});
+
 gulp.task("uglify", ["browserify"], function () {
   return gulp.src("./dist/piii.js")
-    .pipe(uglify({output: {comments: "/^!.*$/"}}))
+    .pipe(uglify({output: {comments: "/^!/"}}))
     .pipe(rename("piii.min.js"))
     .pipe(gulp.dest("./dist/"));
 });
@@ -44,8 +44,6 @@ gulp.task("watch", function () {
   });
 });
 
-
 gulp.task("default", [
-  "uglify",
-  "regexs"
+  "uglify"
 ]);
