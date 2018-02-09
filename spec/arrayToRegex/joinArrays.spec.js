740d458f8084ce6bab29ab42de5b7ecb3ecaab4a
Matheus Alves <theuves@outlook.com>
2018-02-09 17:26:21 -0300

    atualiza testes de src/arrayToRegex/joinArrays.js

--- spec/arrayToRegex/joinArrays.spec.js
+++ spec/arrayToRegex/joinArrays.spec.js
@@ -6,4 +6,7 @@ describe("arrayToRegex/joinArrays", () => {
   it("deve juntar uma array invertendo e separando por '|'", () => {
     expect(joinArrays(["foo", "bar", "baz"])).toBe("(baz|bar|foo)");
   });
+  it("deve remover todos os valores repetidos", () => {
+    expect(joinArrays(["foo", "bar", "foo"])).toBe("(bar|foo)");
+  });
 });
