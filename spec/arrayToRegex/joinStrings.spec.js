230206b487e8dfe6311715cca09ba7cc17cf3edf
Matheus Alves <theuves@outlook.com>
2018-02-09 17:32:06 -0300

    atualiza testes de src/arrayToRegex/joinStrings.js

--- spec/arrayToRegex/joinStrings.spec.js
+++ spec/arrayToRegex/joinStrings.spec.js
@@ -6,4 +6,11 @@ describe("arrayToRegex/joinStrings", () => {
   it("deve juntar uma array deixando em ordem decrescente e separando por '|'", () => {
     expect(joinStrings(["a", "b", "c"])).toBe("(c|b|a)");
   });
+  it("deve deixar todos os valores em caixa baixa", () => {
+    expect(joinStrings(["A", "B", "C"])).toBe("(c|b|a)");
+  });
+  it("deve remover todos os valores repetidos", () => {
+    expect(joinStrings(["a", "b", "a"])).toBe("(b|a)");
+    expect(joinStrings(["A", "b", "a"])).toBe("(b|a)");
+  });
 });
