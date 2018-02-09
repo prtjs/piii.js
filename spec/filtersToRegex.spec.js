df28aed056dc89396dc5d76e55a3580bc6f7c958
Matheus Alves <theuves@outlook.com>
2018-02-09 16:25:38 -0300

    atualiza testes com regexs

--- spec/filtersToRegex.spec.js
+++ spec/filtersToRegex.spec.js
@@ -13,15 +13,15 @@ describe("filtersToRegex", () => {
     expect(filtersToRegex([], false, {})).toEqual(/(?:)/);
   });
   it("deve criar um regex sem repetição e sem aliases", () => {
-    expect(filtersToRegex(filters, false, {})).toEqual(/\b((foo|bar))\b/g);
+    expect(filtersToRegex(filters, false, {})).toEqual(/\b((foo|bar))\b/gi);
   });
   it("deve criar um regex com repetição", () => {
-    expect(filtersToRegex(filters, true, {})).toEqual(/\b((f+o+o+|b+a+r+))\b/g);
+    expect(filtersToRegex(filters, true, {})).toEqual(/\b((f+o+o+|b+a+r+))\b/gi);
   });
   it("deve criar um regex sem repetição e com aliases", () => {
-    expect(filtersToRegex(filters, false, {a: ["4"]})).toEqual(/\b((foo|b[a4]r))\b/g);
+    expect(filtersToRegex(filters, false, {a: ["4"]})).toEqual(/\b((foo|b[a4]r))\b/gi);
   });
   it("deve criar um regex com repetição e com aliases", () => {
-    expect(filtersToRegex(filters, true, {a: ["4"]})).toEqual(/\b((f+o+o+|b+[a4]+r+))\b/g);
+    expect(filtersToRegex(filters, true, {a: ["4"]})).toEqual(/\b((f+o+o+|b+[a4]+r+))\b/gi);
   });
 });
